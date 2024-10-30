7a6b6764e4b8802575bb02ee0439b55e1c7dbbb6
dnvnaz <donovanazevedo@gmail.com>
2024-10-30 16:03:40 -0400

    criacao lista de tarefas

---
+++ src/main/java/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,19 @@
+package main.java.list.OperacoesBasicas;
+
+public class Tarefa {
+
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
